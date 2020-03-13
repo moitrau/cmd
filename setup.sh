7b958383f18e4f43902ff2da60a86381155d1df0
moitrau <62080348+moitrau@users.noreply.github.com>
2020-03-13 19:00:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,14 @@ cd usersnjapp
 git init
 git remote add -f origin https://github.com/moitrau/usersnjapp.git
 git pull origin master
+
+cd /root/repo/usersnjapp
+docker-compose up -d
+
+docker pull dpage/pgadmin4
+docker run -p 80:80  \
+    -e 'PGADMIN_DEFAULT_EMAIL=pgadmin' \
+    -e 'PGADMIN_DEFAULT_PASSWORD=pgpass' \
+    -d dpage/pgadmin4
+
+
